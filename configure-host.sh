4b1cedf8bc823c74357a14fd4f47699d26fca0e1
Karl Podger <karl.podger@primeordinal.com>
2026-02-10 16:47:06 +0000

    fix: Use Caddy template

--- configure-host.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-#  Run this on the host to configure the site to be served on coffee1-website.dev.redgrain.co.uk
-sudo bash -c 'echo "127.0.0.1 coffee1-website.dev.redgrain.co.uk" >> /etc/hosts' && echo "✓ Added coffee1-website.dev.redgrain.co.uk to /etc/hosts"
